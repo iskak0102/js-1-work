bdd0f96eca4c4a21ac2007dc813fdbb7dd3e96db
iskak0102 <iskakm@bk.ru>
2023-11-03 16:56:51 +0600

    first commit

---
+++ work/script.js
@@ -0,0 +1,28 @@
+//boolean=true/false
+//string=''/""
+//number=1
+//undefined
+//null
+//symbol
+//bigInt
+
+//let a=4
+//let b=55
+//console.log(a+b)
+//console.log(a*b)
+//console.log(a/b)
+//console.log(a%b)
+
+
+//let a='hello'
+//let b='Iskak'
+//console.log(a+b)
+//console.log(a-b)
+
+//let a='5'
+//let b=5
+//console.log(+a+b)
+
+//let c=2
+//let b
+//console.log(b)
